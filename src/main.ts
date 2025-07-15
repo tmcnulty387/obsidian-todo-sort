31e846337eb1f56e766a3ab819a0a3e096723040
tmcnulty387 <tmcnulty387@gmail.com>
2025-07-14 23:34:15 -0400

    Optimized main.ts to trigger sort only on checkbox change

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
-import { debounce, Editor, MarkdownView, Plugin } from "obsidian";
+import { Editor, MarkdownView, Plugin } from "obsidian";
 import { DEFAULT_SETTINGS, Settings, SettingsTab } from "./settings";
 import { sortTodos } from "./sort";
 
 export default class MyPlugin extends Plugin {
   settings: Settings;
-  _debouncedSortTodos: (editor: Editor) => void;
 
   async onload() {
     await this.loadSettings();
-    this._updateDebounce();
     this.addSettingTab(new SettingsTab(this.app, this));
     this.registerEvent(
       this.app.workspace.on("editor-change", this._onEditorChange)
@@ -23,19 +21,65 @@ export default class MyPlugin extends Plugin {
 
   async saveSettings() {
     await this.saveData(this.settings);
-    this._updateDebounce();
   }
 
+  _previousEditorValue = "";
   _onEditorChange = (editor: Editor, _markdownView: MarkdownView) => {
-    this._debouncedSortTodos(editor);
+    const newValue = editor.getValue();
+    const oldValue = this._previousEditorValue;
+    this._previousEditorValue = newValue;
+
+    if (!oldValue) {
+      return;
+    }
+
+    const newLines = newValue.split("\n");
+    const oldLines = oldValue.split("\n");
+    if (newLines.length !== oldLines.length) {
+      return;
+    }
+
+    let changedLineIndex = -1;
+    for (let i = 0; i < newLines.length; i++) {
+      if (newLines[i] !== oldLines[i]) {
+        if (changedLineIndex !== -1) {
+          return; // More than one line changed
+        }
+        changedLineIndex = i;
+      }
+    }
+
+    if (changedLineIndex === -1) {
+      return; // No change
+    }
+
+    const newLine = newLines[changedLineIndex];
+    const oldLine = oldLines[changedLineIndex];
+
+    const todoRegex = /^(\s*- \[)(.)(\]\s*.*)$/;
+    const newLineMatch = newLine.match(todoRegex);
+    const oldLineMatch = oldLine.match(todoRegex);
+
+    if (newLineMatch && oldLineMatch) {
+      const [_n, newLinePrefix, newLineCheck, newLineSuffix] = newLineMatch;
+      const [_o, oldLinePrefix, oldLineCheck, oldLineSuffix] = oldLineMatch;
+
+      if (
+        newLinePrefix === oldLinePrefix &&
+        newLineSuffix === oldLineSuffix &&
+        newLineCheck !== oldLineCheck
+      ) {
+        this._sortTodos(editor);
+      }
+    }
   };
 
   _lastSort = new Date();
-  _lastValue = "";
+  _lastSortedValue = "";
   _sortTodos = (editor: Editor) => {
     const began = new Date();
     const value = editor.getValue();
-    if (value === this._lastValue) {
+    if (value === this._lastSortedValue) {
       return;
     }
     if (new Date().getTime() - this._lastSort.getTime() < 100) {
@@ -49,7 +93,8 @@ export default class MyPlugin extends Plugin {
       const now = new Date();
       console.log(`Sorted todos in ${now.getTime() - began.getTime()}ms`);
       this._lastSort = now;
-      this._lastValue = result.output;
+      this._lastSortedValue = result.output;
+      this._previousEditorValue = result.output;
       editor.setValue(result.output);
       const newLine = result.lineMap[lineNumber];
       editor.setCursor({
@@ -57,13 +102,5 @@ export default class MyPlugin extends Plugin {
         ch: cursor.ch,
       });
     }
-  };
-
-  _updateDebounce = () => {
-    this._debouncedSortTodos = debounce(
-      this._sortTodos,
-      this.settings.delayMs,
-      true
-    );
   };
 }
